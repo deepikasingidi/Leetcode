a534ae8e5594edb4502448cb44d35e6eecec9150
deepikasingidi <147781264+deepikasingidi@users.noreply.github.com>
2025-12-25 21:21:42 +0530

    Time: 156 ms (60.99%), Space: 107.8 MB (65.96%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.begin(), happiness.end(), greater<int>());
+        int n = happiness.size();
+        long long ans = 0;
+        for(int i=0; i<k; i++){
+            ans += max(0, happiness[i]-i);
+        }
+        return ans;
+    }
+};
