fbd20eacbac08f431c2ff7019330681ea3b45014
deepikasingidi <147781264+deepikasingidi@users.noreply.github.com>
2026-07-22 10:37:43 +0530

    Time: 572 ms (12.89%), Space: 296.4 MB (21.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int fun(vector<int>&arr, int n, int i, int prev) {
+        if(i == n) return 0;
+
+        if(dp[i][prev+1] != -1) return dp[i][prev+1];
+
+        int pick = 0;
+        if(prev==-1 || arr[i]>arr[prev]) pick = 1 + fun(arr, n, i+1, i);
+        int non_pick = fun(arr, n, i+1, prev);
+
+        dp[i][prev+1] = max(pick, non_pick);
+
+        return dp[i][prev+1];
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
-        ans.push_back(nums[0]);
-        for(int i=1; i<n; i++) {
-            if(ans.back() < nums[i]) {
-                ans.push_back(nums[i]);
-            }
-            else {
-                int lb = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
-                ans[lb] = nums[i];
-            }
-        }
-        return ans.size();
+        dp.resize(n, vector<int>(n+1, -1));
+        int cnt = fun(nums, n, 0, -1);
+        
+        return dp[0][0];
     }
 };
